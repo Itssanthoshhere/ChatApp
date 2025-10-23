e6dfe38b1db97ec05f7d0f2c4a6bc3846cf5c628
Santhosh VS <santhosh02vs@gmail.com>
2025-10-23 05:46:37 +0530

    Add custom icons and styles to bottom tab navigation - Integrated Ionicons and MaterialCommunityIcons for tab icons - Applied focused state styling with background highlight and color change - Adjusted tabBar height and label styles for better UI

--- chatapp/app/(tabs)/_layout.tsx
+++ chatapp/app/(tabs)/_layout.tsx
@@ -1,8 +1,60 @@
 import { Tabs } from "expo-router";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { View } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          height: 60,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          color: "black",
+        },
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+          let title;
+          let IconComponent = Ionicons;
+
+          switch (route.name) {
+            case "chats":
+              iconName = "chatbubble";
+              title = "Chats";
+              break;
+            case "updates":
+              IconComponent = MaterialCommunityIcons;
+              iconName = "update";
+              title = "Updates";
+              break;
+            case "communities":
+              iconName = "people";
+              title = "Communities";
+              break;
+            case "calls":
+              iconName = "call";
+              title = "Calls";
+              break;
+
+            default:
+              return null;
+          }
+
+          const iconColor = focused ? "#075E54" : "black";
+
+          return (
+            <View className=" w-[100px] h-[58px] items-center justify-center">
+              <View
+                className={`${focused ? "bg-green-200" : "bg-transparent"}  px-5 py-1.5 rounded-full relative`}
+              >
+                <IconComponent size={18} name={iconName} color={iconColor} />
+              </View>
+            </View>
+          );
+        },
+      })}
+    >
       <Tabs.Screen name="chats" options={{ title: "Chats" }} />
       <Tabs.Screen name="updates" options={{ title: "Updates" }} />
       <Tabs.Screen name="communities" options={{ title: "Communities" }} />
